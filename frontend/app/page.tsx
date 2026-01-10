931a72901af49f4099bec7cf87ae98a00217b5c8
Vijayakumar R <vijay@tectratechnologies.com>
2026-01-10 15:09:44 +0530

    2nd test commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,6 +16,9 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
+          <h3 className="text-lg font-semibold leading-8 text-zinc-600 dark:text-zinc-400">
+            Hello World
+          </h3>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
