7f9b63277789e818d1327a7770be50a576f29229
Akash <akash.2309911@gmail.com>
2026-01-20 14:57:22 +0530

    Changes

--- frontend/components/antiragging/AntiRaggingContent.tsx
+++ frontend/components/antiragging/AntiRaggingContent.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { ChevronLeft } from "lucide-react";
+
 
 /**
  * Recreating the exact design fidelity requested by the user.
